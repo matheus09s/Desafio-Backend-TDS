5892e961c796a8813bc752911fbe881f891f34b8
Matheus <matheus.h.oliveira0911@gmail.com>
2024-06-13 01:46:11 -0300

    initial config

---
+++ src/main/java/tds/company/api/controller/UrlController.java
@@ -0,0 +1,13 @@
+package tds.company.api.controller;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UrlController {
+
+
+}
