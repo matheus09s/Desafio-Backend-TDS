1ff4edb3f288357c007a8f10ffc0e82864069c90
Matheus <matheus.h.oliveira0911@gmail.com>
2024-06-13 12:57:24 -0300

    changing the service class

--- src/main/java/tds/company/api/service/UrlService.java
+++ src/main/java/tds/company/api/service/UrlService.java
@@ -1,21 +1,38 @@
 package tds.company.api.service;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import tds.company.api.dto.ShortenUrlRequest;
-import tds.company.api.entity.Url;
+import tds.company.api.entity.UrlEntity;
 import tds.company.api.repository.UrlRepository;
 
+@Service
 public class UrlService {
 
-    @Autowired
     UrlRepository urlRepository;
 
 
+    public UrlService(UrlRepository urlRepository) {
+        this.urlRepository = urlRepository;
+    }
 
     public String convertShortUrl(ShortenUrlRequest shortenUrlRequest) {
-        Url url = new Url();
 
-        url.
+        String id;
+        do {
+            id = RandomStringUtils.randomAlphanumeric(5, 10);
+        } while (urlRepository.existsById(id));
+
+
+        urlRepository.save(new UrlEntity(id, shortenUrlRequest.getLongUrl()));
+
+
+        return id;
+
+
+
+
 
 
     }
