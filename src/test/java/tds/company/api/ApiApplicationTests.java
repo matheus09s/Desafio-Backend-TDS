8801d58a21a213b8dae959e979dca1b7e5a23d6f
Matheus <matheus.h.oliveira0911@gmail.com>
2024-06-14 13:53:41 -0300

    creating tests

--- src/test/java/tds/company/api/ApiApplicationTests.java
+++ src/test/java/tds/company/api/ApiApplicationTests.java
@@ -1,13 +1,69 @@
 package tds.company.api;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.hamcrest.Matchers.startsWith;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+
+// ... seu código de teste
 @SpringBootTest
+@AutoConfigureMockMvc
 class ApiApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	public void cadastrarUrl() throws Exception {
+		String longUrl = "https://example.com/very/long/url";
+		String requestBody = "{\"longUrl\":\"" + longUrl + "\"}";
+
+		mockMvc.perform(post("/shorten-url")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(requestBody))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.shortUrl", startsWith("http://localhost/")));
 	}
+
+	@Test
+	public void verificarDirecionamento() throws Exception {
+		String shortUrlId = "0rDtF";
+		String longUrl = "https://example.com/very/long/url";
+
+		mockMvc.perform(MockMvcRequestBuilders.get("/" + shortUrlId))
+				.andExpect(status().isFound())
+				.andExpect(header().string("Location", longUrl));
+	}
+
+	@Test
+	public void verificarTotalAcessosMedia() throws Exception {
+		String shortUrlId = "0rDtF";
+
+		mockMvc.perform(MockMvcRequestBuilders.get("/stats/" + shortUrlId))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.totalAccesses", is(1))) // Exemplo de total de acessos
+				.andExpect(jsonPath("$.averageAccessesPerDay", is(1.0))); // Exemplo de média de acessos por dia
+	}
+
+	@Test
+	public void verificarId() throws Exception {
+		String nonExistentShortUrlId = "nonExistent";
+
+		mockMvc.perform(MockMvcRequestBuilders.get("/" + nonExistentShortUrlId))
+				.andExpect(status().isNotFound());
+	}
+
+
+
 
 }
